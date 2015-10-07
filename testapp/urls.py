3583bf5241e543a327a0d4fe3767df0f9a17436b
kamilnazar <kamil@papercrane.in>
2015-10-07 10:02:42 +0530

    url modified

--- testapp/urls.py
+++ testapp/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^medicines', views.MedicineList.as_view()),
-    url(r'^diseases', views.DiseaseList.as_view()),
-    url(r'^manufacturers', views.ManufacturerList.as_view()),
-    url(r'^kalpana', views.KalpanaList.as_view()),
+    url(r'^medicines/$', views.MedicineList.as_view()),
+    url(r'^diseases/$', views.DiseaseList.as_view()),
+    url(r'^manufacturers/$', views.ManufacturerList.as_view()),
+    url(r'^kalpana/$', views.KalpanaList.as_view()),
 ]
